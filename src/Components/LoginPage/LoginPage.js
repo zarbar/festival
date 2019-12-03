5a81aae1d79d998083f887d0e5dec40d3e8ada2a
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-03 09:02:56 +0000

    Google Login Button

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './LoginPage.css';
+import GoogleLoginButton from '../../Zara_Google_Login/GoogleLoginButton';
 
 class LoginPage extends React.Component {
 
     render() {
         return (
             <>
-
+                <br />
+                <GoogleLoginButton />
+                <br />
             </>
         )
     }
---
+++ src/Zara_Google_Login/GoogleLoginButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+
+
+
+export default function GoogleLoginButton() {
+    return (
+        <>
+            <GoogleLogin
+                clientId="938737799020-75i5e0rtpiljimr4lqlmq4uticm2c9bn.apps.googleusercontent.com"
+                buttonText="Login with Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+        </>
+    )
+}
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
+
+
+  //client ID = 938737799020-75i5e0rtpiljimr4lqlmq4uticm2c9bn.apps.googleusercontent.com
+
+
+  //client secret = 
+//   8q5xWtfYsuCjmpVBv58dQXat
