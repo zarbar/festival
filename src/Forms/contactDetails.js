bab4c8a2e9148d1e9a29e552ad27671b582ae972
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-04 15:31:02 +0000

    changes

--- src/Forms/contactDetails.js
+++ src/Forms/contactDetails.js
@@ -6,10 +6,10 @@ export default function ContactDetails() {
         <h1>Contact Details for Your Event</h1>
         <h3>Remember, these are public!</h3>
         <form>
-            <label>Twitter Handle:</label><input type="text" required></input>
-            <label>Facebook Page:</label> <input type="text"required></input>
-            <label>Contact Email:</label><input type="text" required></input>
-            <label>Phone Number:</label><input type="text"></input>
+            <label>Twitter Handle:</label><input type="text" required placeholder="@myfestival"></input>
+            <label>Facebook Page:</label> <input type="text"required placeholder="Your Festival"></input>
+            <label>Contact Email:</label><input type="text" required placeholder="contact@crazyfest.com"></input>
+            <label>Phone Number:</label><input type="text" placeholder="+44..."></input>
         </form>
         <button className="item">Continue</button>
         </>
--- src/Forms/signupForm.js
+++ src/Forms/signupForm.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signupFormStyle.css'
 
 export default function SignUp() {
+    let [name, setName] = useState('');
+    function handleNameChange(e) {
+        setName(<h3>Your festival {e.target.value} is just a few clicks away, let's get this party started!</h3>)
+    }
     return (
         <>
         <button>Login With Facebook</button>
+        {name}
         <form>
-            <input type="text" placeholder="Festival name" required></input>
+            <input type="text" placeholder="Festival name" required onChange={handleNameChange}></input>
             <input type="text" placeholder="Email" required></input>
             <input type="text" placeholder="Password" required></input>
             <input type="text" placeholder="Confirm assword" required></input>
