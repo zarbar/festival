adabf1856836630e067a6b6871ee0a262812ecb8
ChristopherLynch31 <44901727+ChristopherLynch31@users.noreply.github.com>
2019-12-06 11:47:50 +0000

    Christopher (#21)
    * Sign up page design
    
    * login page styling
    
    * Dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -6,9 +6,16 @@ class Dashboard extends React.Component {
 render() {
     return (
 <>
-This is the Dashboard component
+        <h1>Your Dashboard</h1>
+        <div className='container'>
+        <div className='item getting'><a href = '../../map.js'>Getting there (location map)</a></div>
+        <div className='item edit'><a href = '../../Forms/EventDescription.js'>Edit schedule</a></div>
+        <div className='item ticket'>Ticket hub</div>
+        <div className='item contact'><a href = '../ContactPage/ContactPage.js'>Contact details</a></div>
+        </div>
+        <div className='preview'>Preview my site</div>
 </>
     )
 }
 }
-export default Dashboard; 
+export default Dashboard;
