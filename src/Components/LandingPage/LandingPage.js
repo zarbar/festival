b1b370619e4af5c0d0a8c47cfe5350cbd91fce53
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-06 11:50:12 +0000

    zaraFriday

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './LandingPage.css';
-// import logo from '../../images/Logos/purpleEnglebert.png';
 import logo1 from '../../images/Logos/purpleEnglebert.png';
 import AboutPage from '../AboutPage/AboutPage';
 
@@ -38,45 +37,8 @@ class LandingPage extends React.Component {
                 <div className='footer-end'>
                     <p>KODIRI 2019</p>
                 </div>
-
-                {/* 
-                <header className="app-header">
-                    <a href="/login"><span>Login</span></a>
-                    <span><img src={logo} alt='logo' /></span>
-                    <span>Menu</span>
-                </header> */}
-
             </>
         )
     }
 }
 export default LandingPage;
-
-
-{/* <main className="wrapper">
-                    <div className='join-now'>
-                        <p>FestiPal is the essential digital tool for creating bespoke, user-friendly web-apps.
-            <br />
-                            Designed for festival organisers, from indie events to international parties.
-            </p>
-                        <br /><br />
-                        <a href="/signup"><button className='join'>Join now</button></a>
-                    </div>
-                </main>
-
-                < AboutPage />
-                <footer>
-                    <div className='logo1'>
-                        <img src={logo1} alt='logo1' />
-                    </div>
-                    <div className='footer-nav'>
-                        <p>Get started</p>
-                        <p>About</p>
-                        <p>Login</p>
-                    </div>
-
-                </footer>
-
-                <div className='footer-end'>
-                    <p>KODIRI 2019</p>
-                </div> */}
