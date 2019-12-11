2358db11b75602c7938b270d47e3ea10ad26386e
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-11 12:52:39 +0000

    a

--- src/Forms/EventDescription.js
+++ src/Forms/EventDescription.js
@@ -12,13 +12,12 @@ export default function EventDescription() {
         <form>
         <label>Event Details: (Don't worry, you can come back to this later!)</label>
         <textarea placeholder="Enter the details of your event here. Explain what people can expect,
-        and why they should be there!"></textarea>
+        and why they should be there"></textarea>
         <label>Start Date:</label><input type="date"/>
-        <label>Start Time:</label><input type="time"/>
         <label>End Date:</label><input type="date"/>
-        <label>End Time:</label><input type="time"/>
-        <label>Precise Location: </label>
-        <label>Approx Location: </label>
+        <label>Location: </label><input type="string" placeholder="Sofia, Bulgaria"/>
+        <label>Enter a short blurb about your festival: </label>
+        <textarea placeholder="Hogsozzle is a perfoming arts and music festival known for its hog rost"></textarea>
         </form>
         <a href="/contactdetails"><button className="item" onClick={handleSubmit}>Continue</button></a><br></br>
         {submit}
