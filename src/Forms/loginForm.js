2072da4a8c13d10aaaa6b2466cb7bf427cd3ecf4
ChristopherLynch31 <44901727+ChristopherLynch31@users.noreply.github.com>
2019-12-11 16:33:06 +0000

    Form styling (#25)
    Nice werk Chris!

--- src/Forms/loginForm.js
+++ src/Forms/loginForm.js
@@ -5,6 +5,10 @@ import GoogleLoginButton from '../Components/GoogleLogin/GoogleLoginButton';
 export default function Login() {
     return (
         <>
+        <br />
+        <GoogleLoginButton text="Log In with Google" />
+        <p id="or"><span>or</span></p>
+        <p>Log in with your email address</p>
             <form>
                 <input type="text" placeholder="email"></input>
                 <input type="text" placeholder="password"></input>
@@ -12,7 +16,6 @@ export default function Login() {
 
             <div className="container">
                 <button className="item">Login</button>
-                <GoogleLoginButton text="Log In with Google" />
                 <em>Don't have an account yet?</em>
                 <a href="/signup"><button className="item">Create New Account</button></a>
             </div>
