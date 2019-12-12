f162890dc4ae59d078523d283c3c5c02a90c6928
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-12 12:58:41 +0000

    trying to get google to work

--- src/Components/GoogleLogin/GoogleLoginButton.js
+++ src/Components/GoogleLogin/GoogleLoginButton.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import axios from 'axios';
 import GoogleLogin from 'react-google-login';
 
-export default function GoogleLoginButton() {
+export default function GoogleLoginButton(props) {
 
-    const googleOAuth = () => {
-        axios('/auth/google');
-    }
     const responseGoogle = (response) => {
         console.log(response);
     }
 
-    return (<>
-        {/* <button onClick={() => googleOAuth()}>
-            Sign-in with Google
-        </button> */}
-        <GoogleLogin
-            clientId="938737799020-b5taf86p2fcjq00f17je4i80sl0qr75c.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={googleOAuth}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-        />
-    </>)
-};
+    return (
+        <>
+            <GoogleLogin
+                clientId="938737799020-75i5e0rtpiljimr4lqlmq4uticm2c9bn.apps.googleusercontent.com"
+                buttonText={props.text}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+        </>
+    )
+}
+
+
+//real one:
+  //client ID = 938737799020-75i5e0rtpiljimr4lqlmq4uticm2c9bn.apps.googleusercontent.com
+  //client secret = 
+//   8q5xWtfYsuCjmpVBv58dQXat
+
+
+//test site:
+// clientId="938737799020-b5taf86p2fcjq00f17je4i80sl0qr75c.apps.googleusercontent.com"
