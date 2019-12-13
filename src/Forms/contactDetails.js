d8b8f57b9ff28535b1618cba9788052e959af469
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-13 13:24:38 +0000

    ready for deploy

--- src/Forms/contactDetails.js
+++ src/Forms/contactDetails.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ContactDetails() {
+    let [twitter, setTwitter] = useState('')
+    // function handleChangeTwitter(e) {
+    //     setTwitter(e.target.value)
+    // }
+    // console.log(twitter);
     return (
         <>
         <h1>Contact Details for Your Event</h1>
         <h3>Remember, these are public!</h3>
         <form>
-            <label>Twitter Handle:</label><input type="text" required placeholder="@myfestival"></input>
-            <label>Facebook Page:</label> <input type="text"required placeholder="Your Festival"></input>
-            <label>Contact Email:</label><input type="text" required placeholder="contact@crazyfest.com"></input>
-            <label>Phone Number:</label><input type="text" placeholder="+44"></input>
+            <label>Twitter Handle:</label><input type="text" required placeholder="@myfestival" className="Twi"></input>
+            <label>Facebook Page:</label> <input type="text"required placeholder="Your Festival" value=""></input>
+            <label>Contact Email:</label><input type="text" required placeholder="contact@crazyfest.com" value=""></input>
+            <label>Phone Number:</label><input type="text" placeholder="+4412345 67890" value=""></input>
         </form>
-        <a href="/dashboard/:userId"><button className="item">Continue</button></a>
+        <a href="/dashboard/:userId"><button className="item" onClick={setTwitter()}>Continue</button></a>
         </>
     )
 }
