38dc0466f44fbb07d9e67d694add2eea970de07f
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-15 12:49:07 +0000

    tidied components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Login from './Components/Forms/loginForm.js';
 import SignUp from './Components/Forms/signupForm.js';
 import EventDescription from './Components/Forms/EventDescription.js';
 import ContactDetails from './Components/Forms/contactDetails.js';
-// import LandingPage from './Components/LandingPage/LandingPage.js';
+import LandingPage from './Components/LandingPage/LandingPage.js';
 import AboutPage from './Components/AboutPage/AboutPage.js';
-import ContactPage from './Components/ContactPage/ContactPage.js';
 import Dashboard from './Components/Dashboard/Dashboard.js';
 import Navbar from './Components/Navbar/Navbar';
 import PreviewSite from './PreviewSite/PreviewSite';
@@ -20,35 +19,46 @@ class App extends Component {
       <Router>
         <div className='App'>
           <Switch>
+            {/* Landing Page */}
             <Route exact path='/'>
-              {/* <Navbar /> */}
-              <FaqForm/>
-              {/* <LandingPage /> */}
+              <Navbar />
+              <LandingPage />
             </Route>
+            {/* About page */}
             <Route exact path='/about'>
               <Navbar />
               <AboutPage />
-              <ContactPage />
             </Route>
+            {/* user dashboard */}
             <Route exact path='/dashboard/:userId'>
               <Navbar />
               <Dashboard />
             </Route>
+            {/* login page/form */}
             <Route exact path='/login'>
+              <Navbar />
               <Login />
             </Route>
+            {/* signup page/form */}
             <Route exact path='/signup'>
+              <Navbar />
               <SignUp />
             </Route>
-            <Route exact path='/loggedInTrue'>
+            {/* event details form */}
+            <Route exact path='/eventDetailsForm'>
               <EventDescription />
             </Route>
-            <Route exact path='/contactdetails'>
+            {/* event contact form */}
+            <Route exact path='/contactForm'>
               <ContactDetails />
             </Route>
+            {/* FAQ form */}
+            <Route exact path='/faqForm'>
+              <FaqForm />
+            </Route>
+            {/* Preview Site */}
             <Route exact path='/PreviewSite'><PreviewSite /></Route>
           </Switch>
-
         </div>
       </Router>
     );
