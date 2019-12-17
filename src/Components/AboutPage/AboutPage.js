7497c8b079858bd17072dedabe9527ea674d36bc
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-17 11:37:42 +0000

    google OAuth connects to personal dashboard page

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -8,11 +8,10 @@ class AboutPage extends React.Component {
         return (
             <>
                 <div className='about'>
-                    <h1>The What</h1>
+                    <h1>About FestiPal</h1>
                     <p>
                         FestiPal is a tool for event organisers to create web-apps for festivals. Simply select a design template, add your event information and build a stylish, reliable site that can be easily updated. FestiPal sites are responsive for mobile and desktop, meaning that your event site will look great across all devices.
                     </p>
-                    <h1>The Why</h1>
                     <p>
                         FestiPal is free and easy to use giving you the power to update content as often and as quickly as needed. You now have one less job in organising your festival.
             </p>
