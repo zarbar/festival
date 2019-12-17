08b6c2d9ad273ea39037562380549c7490cddce7
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-12-17 09:24:24 +0000

    merge take 435

--- src/Forms/contactDetails.js
+++ src/Forms/contactDetails.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 
 export default function ContactDetails() {
-    let [twitter, setTwitter] = useState('')
-    // function handleChangeTwitter(e) {
-    //     setTwitter(e.target.value)
-    // }
-    // console.log(twitter);
+    let [twitter, setTwitter] = useState("");
+    function twitterChange(e) {
+    localStorage.setItem('Twitter', e.target.value)
+    }
+    console.log(localStorage)
     return (
         <>
         <h1>Contact Details for Your Event</h1>
         <h3>Remember, these are public!</h3>
         <form>
-            <label>Twitter Handle:</label><input type="text" required placeholder="@myfestival" className="Twi"></input>
+            <label>Twitter Handle:</label><input type="text" required onChange={(e) => twitterChange} placeholder="@myfestival" className="Twi"></input>
             <label>Facebook Page:</label> <input type="text"required placeholder="Your Festival" value=""></input>
             <label>Contact Email:</label><input type="text" required placeholder="contact@crazyfest.com" value=""></input>
             <label>Phone Number:</label><input type="text" placeholder="+4412345 67890" value=""></input>
