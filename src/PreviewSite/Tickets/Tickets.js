5c95e573ae8a163568c464141c4124bb7b715ad0
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-18 17:11:24 +0000

    neatening u

--- src/PreviewSite/Tickets/Tickets.js
+++ src/PreviewSite/Tickets/Tickets.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import './Tickets.css'
-
+import React from 'react';
+import './Tickets.css';
+import festival from '../../images/ticket/festival1.jpg';
+import concert from '../../images/ticket/concert.jpg';
+import music from '../../images/ticket/music.jpg';
 
 export default class Tickets extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            ticketDes: 'Super early bird passes on sale now. Get your ticket to Bulgaria’s hottest summer party.',
+            ticketLink: '/',
+            imgSrc1: festival,
+            imgSrc2: concert,
+            imgSrc3: music
+        }
     }
 
-render() {
-    return (
-        <div className='buy-tickets'>
-            <p> Super early bird passes on sale now.<br />
-                Get your ticket to Bulgaria’s hottest<br />
-                summer party.</p>
-            <button className='button'>Buy Tickets</button>
-        </div>
-    );
-}
+    render() {
+        return (
+            <div className='buy-tickets'>
+                <p>{this.state.ticketDes}</p>
+                <button className='button'
+                ><a href={this.state.ticketLink}>Buy Tickets</a></button>
+                <img src={this.state.imgSrc1} alt='your' />
+            </div>
+        );
+    }
 }
