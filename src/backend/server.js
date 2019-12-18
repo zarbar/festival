ab6d5da4fcebdcd4fa9c2dfae2b1539f754276bf
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-18 21:40:31 +0000

    data passes from form to mongoDB

--- src/backend/server.js
+++ src/backend/server.js
@@ -23,17 +23,30 @@ db.connect().then(dbo => {
                 if (currentUser) {
                     console.log('user is: ', currentUser);
                 } else {
-                    let newUser = { username: req.body.username, googleId: req.body.googleId };
+                    let newUser = {
+                        username: req.body.username, googleId: req.body.googleId,
+                        festivalInfo: {}
+                    };
                     dbo.collection('festipalusers').insertOne(newUser, function (err, res) {
                         if (err) throw err;
                         console.log("1 document inserted");
                     })
                 }
-            }
-            )
-    }
-    )
+            })
+    })
 });
+
+db.connect().then(dbo => {
+    app.post('/saveFormData', (req, res) => {
+        let findDoc = { googleId: req.body.googleId };
+        let newData = { $set: { festivalInfo: { basicFormData: req.body.festivalData } } };
+        dbo.collection('festipalusers').updateOne(findDoc, newData, function (err, res) {
+            if (err) throw err;
+            console.log("document updated");
+        })
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
