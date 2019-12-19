e249d4f14dbcda52735a5c91d0220eecf8e3ccb0
Mumtahin Zihan <mumtahin@yahoo.com>
2019-12-19 14:41:32 +0000

    Mumtahin8 (#58)
    * update
    
    * dont touch basic info
    
    * css corrected

--- src/Components/Forms/LoginForm/loginForm.js
+++ src/Components/Forms/LoginForm/loginForm.js
@@ -9,15 +9,14 @@ export default function LoginForm() {
             <p id="or"><span>or</span></p>
             <p>Log in with your email address</p>
             <form>
-                <input type="text" placeholder="email"></input>
-                <input type="text" placeholder="password"></input>
+                <input className='email-input' type="text" placeholder="email"></input>
+                <input className='email-input' type="text" placeholder="password"></input>
             </form>
-
             <div className="container">
-                <button className="item">Login</button>
-                <em>Don't have an account yet?</em>
-                <a href="/signup"><button className="item">Create New Account</button></a>
-            </div>
+                <button className="item">Login</button><br/>
+                <em>Don't have an account yet?</em><br/>
+                <a href="/signup"><button id="item-anchor">Create New Account</button></a>
+            </div><br/>
         </main>
     )
 }
--- src/PreviewSite/Connect/Connect.js
+++ src/PreviewSite/Connect/Connect.js
@@ -3,30 +3,32 @@ import './Connect.css'
 
 
 export default class Connect extends React.Component {
- constructor (props) {
-     super (props)
-     this.state = {
-         socialMediaLink1: 'Twitter',
-         socialMediaLink2: 'Facebook',
-         socialMediaLink3: 'Instagram',
-         contactEmail: 'info@hogsozzle.com',
-         companyAddres: 'your company address'
-     }
- }
+    constructor(props) {
+        super(props)
+        this.state = {
+            socialMediaLink1: 'Twitter',
+            socialMediaLink2: 'Facebook',
+            socialMediaLink3: 'Instagram',
+            contactEmail: 'info@hogsozzle.com',
+            companyAddres: 'your company address'
+        }
+    }
 
- render () {
-     return (
-         <>
-        <div className='connect'> 
-        <h2>Connect with us</h2>
-        </div>
-        <p>{this.state.socialMediaLink1}</p>
-        <p>{this.state.socialMediaLink2}</p>
-        <p>{this.state.socialMediaLink3}</p>
-        Email us:
-        <a className='festivalEmail' href={`mailto:${this.state.contactEmail}`}>{this.state.contactEmail}</a>
-        <p>{this.state.companyAddres}</p>
-    </>
-     );
- }
+    render() {
+        return (
+            <>
+                <div className='connect'>
+                    <h2>Connect with us</h2>
+                </div>
+                <div className='connect-withus'>
+                    <p>{this.state.socialMediaLink1}</p>
+                    <p>{this.state.socialMediaLink2}</p>
+                    <p>{this.state.socialMediaLink3}</p>
+                    Email us:
+                    <a className='festivalEmail' href={`mailto:${this.state.contactEmail}`}>{this.state.contactEmail}</a>
+                    <p>{this.state.companyAddres}</p>
+                </div>
+            </>
+        );
+    }
 }
